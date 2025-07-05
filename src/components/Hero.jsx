58f5ae67744022098070d44ee7caa82af161f56b
Prince <princeku6675@gmail.com>
2025-07-05 21:42:56 +0530

    Second commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+const Hero = () => {
+  return (
+    <section id="hero" className="noisy">
+       <h1 className="title">MOJITO</h1>
+       <img src="/src/assets/images/hero-left-leaf.png" alt="" />
+    </section>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,37 @@
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants";
+import gsap from 'gsap';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+    useGSAP(()=>{
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+        navTween.fromTo('nav',{backgroundColor: 'transparent'}, {backgroundColor: '#00000050', backgroundFilter: 'blur(10px)', duration: 1, ease: 'power1.inOut'})
+    })
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+             <img src="/src/assets/images/logo.png" alt="logo" />
+             <p>Mojito</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <p>{link.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
